3b6f68b48a879802af523f8ba28f111122f29077
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-11-27 20:36:50 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // import express from 'express';
 const express = require('express');
 
-const app = express();
+const app = express();     // basically a function
 
-app.listen(3000);
+// app.listen(3000);    // here we are using the methods of the app express
+                    // listen is a method which listens to the the HTTP requests.
+
+
+app.listen(3000, function(){
+    console.log("Server started Successfuly at port 3000");
+})
