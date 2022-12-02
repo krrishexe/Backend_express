146af53b05d6923d8eacfd13ccd33af647140122
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-12-02 20:41:09 +0530

    updated calculator .js

--- Calculator Backend/Calculator.js
+++ Calculator Backend/Calculator.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+
+app.get("/",function(req,res){
+    console.log(res.sendFile(__dirname)); 
+})
+
+        // If we are working on a big  project and we dont know the exact location of the file , we can use
+        // "__dirname" attribute , it gives us the directory of the current file.
+
 app.listen(3000, function(){
     console.log("server started successfully at port 3000");
 });
