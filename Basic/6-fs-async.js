960eedfd9893643595d0c9ad4c42031d35805ca6
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-16 23:48:02 +0530

    aded asyncFS js

--- Basic/6-fs-async.js
+++ Basic/6-fs-async.js
@@ -1,1 +1,17 @@
-const {readFile,writeFile } = require('fs')
+const {readFile,writeFile } = require('fs');
+
+readFile('./content/first.txt' , 'utf-8' , (err,result)=>{
+    if(err){
+        console.log(err);
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt','utf-8',(err,result)=>{
+        if(err){
+            console.log(err);
+            return
+        }
+        const second = result;
+        writeFile('./content/result-async.txt',`this is a new file : ${first} , ${second}`);
+    })  
+})
