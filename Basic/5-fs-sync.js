e759542587660ccf4e7950a72a7f548921666c14
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-17 12:53:41 +0530

    added comments to the async and sync fs and also done the difference between them

--- Basic/5-fs-sync.js
+++ Basic/5-fs-sync.js
@@ -1,7 +1,11 @@
 const {readFileSync,writeFileSync, } = require('fs')
 
+// console.log("Starting the fs task")
+
 const first = readFileSync('./content/first.txt','utf-8');        // syntax
 const second = readFileSync('./content/second.txt','utf-8');      // syntax
+
+// console.log("creating the writeFileSync")
 
 writeFileSync('./content/result-sync.txt',`this is a new file ${first}, ${second}`)
 //syntax
@@ -9,3 +13,5 @@ writeFileSync('./content/result-sync.txt',`this is a new file ${first}, ${second
 // and if the file is already there , writeFileSync will override the text written there.
 
 console.log(first,second); 
+
+// console.log("Done with the task");
--- Basic/6-fs-async.js
+++ Basic/6-fs-async.js
@@ -1,5 +1,7 @@
 const {readFile,writeFile } = require('fs');
 
+// console.log("creating the task i.e. reading the files")
+
 readFile('./content/first.txt' , 'utf-8' , (err,result)=>{
     if(err){
         console.log(err);
@@ -12,6 +14,19 @@ readFile('./content/first.txt' , 'utf-8' , (err,result)=>{
             return
         }
         const second = result;
-        writeFile('./content/result-async.txt',`this is a new file : ${first} , ${second}`);
+        writeFile('./content/result-async.txt',
+        `this is a new file : ${first} , ${second}`,
+        (err,result)=>{
+            if(err){
+                console.log(err)
+                return
+            }
+            console.log(result)                       // result will be undefined cz writeFile wont know the thing that we wrote on that new file.
+        });
     })  
 })
+
+// console.log("Starting the next task");
+
+// Asynchronous  --> Non Blocking
+// Synchronous  --> Blocking
