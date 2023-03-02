ffa36fcc383a960d2d4135b9efa8c8a37bab01e5
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-02 12:13:13 +0530

    tried scraping imdb , flipkart , nike and superkicks

--- WebScraper/index.js
+++ WebScraper/index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const app = express();
 
-axios('https://www.nike.com/in/launch')                                //axios( url of the site that you wanna scrap)
+axios('https://www.superkicks.in/collections/men-sneakers?filter.v.availability=1&page=1')                                //axios( url of the site that you wanna scrap)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
@@ -12,45 +12,48 @@ axios('https://www.nike.com/in/launch')                                //axios( 
         const para = []
         const imgArticlesLinks = []
         // loading the data recieved from the site to cheerio
-        $(".headline-5", html).each(function () {                      // for each item 
+        // $(".headline-5", html).each(function () {                      // for each item 
 
-            const title = $(this).text();
-                                                    // getting the particular item by --> a(this)
-                                                    // and now converting it to text.
-            heading.push({
-                title
-            })
-        })
+        //     const title = $(this).text();
+        //                                             // getting the particular item by --> a(this)
+        //                                             // and now converting it to text.
+        //     heading.push({
+        //         title
+        //     })
+        // })
 
-        $(".headline-3", html).each(function () {                      // for each item 
+        // $(".headline-3", html).each(function () {                      // for each item 
 
-            const p = $(this).text();
-                                                    // getting the particular item by --> a(this)
-                                                    // and now converting it to text.
-            para.push({
-                p
-            })
-        })
-
-        // $(".headline-3",html).each(function(){
         //     const p = $(this).text();
-
+        //     console.log(p)
+        //                                             // getting the particular item by --> a(this)
+        //                                             // and now converting it to text.
         //     para.push({
         //         p
         //     })
         // })
 
 
-        $(".image-component mod-image-component u-full-width", html).each(function () {                      // for each item 
 
-            const url = $(this).text();
+
+            // ASK ASHU ABOUT THE TWO IMAGES IN A SINGLE DIV .
+
+
+
+        $(".card__media", html).each(function () {                      // for each item 
+
+            // const url = $(this).text();
+            const url = $(this).find('img').attr('src')
+            
+            const url2 = $(this).find('img').attr('srcset')
             // getting the particular item by --> $(this)
             // and now converting it to text.
             imgArticlesLinks.push({
-                url
+                url:'https:'+url,
+                url2:'https:'+url2
             })
         })
-        console.log(heading,para,imgArticlesLinks);
+        console.log(imgArticlesLinks);
         // console.log(heading);
     }).catch(err => console.log(err))
 
