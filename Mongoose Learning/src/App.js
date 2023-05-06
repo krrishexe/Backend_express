dd6928d9be5c11de7a6250677797cf263cb70e0b
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-05-06 22:56:31 +0530

    learning

--- Mongoose Learning/src/App.js
+++ Mongoose Learning/src/App.js
@@ -4,25 +4,33 @@ const {Schema} = mongoose
 const {Model} = mongoose
 
 //CREATING SCHEMA
-const practiceSchema = new Schema({
+const Books = new Schema({
     name:String,
     description:String,
-    date:String,
-    time:String,
-    location:String,
+    author:String,
+    pulishedIn:Date,
 })
 
 
-//ADDING DOCUMENTS
-
-
-
-
 // CREATING COLLECTIONS
-const Prac = new Model("prac",practiceSchema)
+const Book = new Model("Book",Books)
+
+
+
+//ADDING DOCUMETS
+const firstBook = new Book({
+    name:"Three men in a Boat",
+    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the",
+    author:"ankit Kumar",
+    location:"rajasthan",
+})
+
+
+firstBook.save();
+
 
 
 //CONNECTING TO DATABASE
-mongoose.connect("mongodb://0.0.0.0:27017/practice")
+mongoose.connect("mongodb://0.0.0.0:27017/Library")
 .then(()=>console.log("connection established"))
 .catch((error)=>console.log(error))
