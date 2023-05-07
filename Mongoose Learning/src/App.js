58d7e1368780eb8a6cd4fca5b15d0c58d0f85375
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-05-07 11:40:53 +0530

    mongoose complete

--- Mongoose Learning/src/App.js
+++ Mongoose Learning/src/App.js
@@ -5,6 +5,8 @@ const { Schema } = mongoose
 
 
 //CONNECTING TO DATABASE
+// ---> if it doesn't connect to the database with localhost , try 0.0.0.0.
+// ---> Here "Library" is the name of the database.
 mongoose.connect("mongodb://0.0.0.0:27017/Library")
     .then(() => console.log("connection established"))
     .catch((error) => console.log(error))
@@ -19,12 +21,13 @@ const booksSchema = new Schema({
 })
 
 
-// CREATING COLLECTIONS
+// CREATING MODELS / COLLECTIONS
+// --> Model Name must be singular. For example "Book".
 const Book = new mongoose.model("Book", booksSchema)
 
 
 
-//ADDING DOCUMET            --->> IT WILL ADD ONLY ONE DOCUMENT AT A TIME
+//ADDING DOCUMET           
 const createDocument = async () => {
     const firstBook = new Book({
         name: "Three men in a Boat",
@@ -32,24 +35,25 @@ const createDocument = async () => {
         author: "ankit Kumar",
         publishedIn: Date.now(),
     })
-
+    // --->> IT WILL ADD ONLY ONE DOCUMENT AT A TIME
+    //SAVING THE FIRST BOOK
+    // const result = await firstBook.save()
+    // console.log(result)
+    
     const secondBook = new Book({
         name: "Invisible man",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the",
         author: "ankit Kumar",
         publishedIn: Date.now(),
     })
-
+    
     const thirdBook = new Book({
         name: "Fifty shades",
         description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the",
         author: "ankit Kumar",
         publishedIn: Date.now(),
     })
-
-    //SAVING THE FIRST BOOK
-    // const result = await firstBook.save()
-    // console.log(result)
+    
 
 
     //SAVING All THE THREE DOCUMENT
@@ -59,10 +63,18 @@ const createDocument = async () => {
 
 //CREATING DOCUMENT
 // createDocument();
+//--->>METHODS
+//.save()
+//.insertOne()
+//.insertMany()
 
 
 
 //READING DOCUMENT
+//-->> METHODS --> 
+//.find()
+//.findOne()
+
 const readingDocument = async () => {
     //TO VIEW ALL THE DOCUMENTS
     const result = await Book.find()
@@ -75,3 +87,11 @@ const readingDocument = async () => {
     // console.log(result);
 }
 // readingDocument()
+
+
+
+
+// db.collection.find()  -- to view/show all the data
+// db.collection.find({ name:"Fifty"}) --> here name is the query.
+// db.collection.find({ name:"Fifty"}).limit(1) --> limiting to show the first document only.
+// db.collection.find({ name:"Fifty"}).limit(1).skip(1) --> showing only one documnet while skipping the first document.
