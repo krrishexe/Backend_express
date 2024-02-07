2e6856e7f776410910df9947fe1410b6f98893de
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-02-07 16:06:16 +0530

    made auth middleware and logout controller

---
+++ AdvancedBackend/src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+
+    try {
+        //req me cookies kaise access kar rahe ho ?
+        // kyuki cookies two way access ho sakti hai , res me bhi or req me bhi.
+        // humne 
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorised Request")
+        }
+
+        const decodedInfo = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedInfo?._id).select("-password -refreshToken")
+        if (!user) {
+            throw new ApiError(401, "Invalid accessToken")
+        }
+
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid accessToken")
+    }
+
+})
