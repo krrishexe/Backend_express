8a422025d42ab6e93b38f2fec9e09334d39f8f49
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-03-17 14:15:04 +0530

    added cors in sockets path

--- socket.io- 101/script.js
+++ socket.io- 101/script.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const socketio = require('socket.io');
 const app = express();
+const cors = require('cors');
 
 app.use(express.static(__dirname + '/public'))
-
+// app.use(cors())
 app.get('/', (req, res) => {
     console.log("hello world!!")
 })
 
 
-const server = app.listen(5000,()=>{
-    console.log("Server is running on port http://localhost:5000/")
+const server = app.listen(5000, () => {
+    console.log("Server is running on port http://localhost:5000")
 })
 
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "http://127.0.0.1:5500",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on('connection', (socket) => {
-    socket.emit('messageFromServer',{data:"Hi from server!!!"})
+    socket.emit('messageFromServer', { data: "Hi from server!!!" })
     socket.on('messageFromClient', (dataFromClient) => {
         console.log(dataFromClient)
     })
-    socket.on('newMessageToServer',(newMsg)=>{
-        io.emit('messageToAllClient',{text:newMsg.data})
+    socket.on('newMessageToServer', (newMsg) => {
+        io.emit('messageToAllClient', { text: newMsg.data })
     })
 })
 
